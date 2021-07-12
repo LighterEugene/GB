954670f484ffc6c1b16661394b8a72cbd3f524f5
Lighter <agniage@list.ru>
2021-07-12 19:11:01 +0300

    /

--- HW7/src/BaseAuthService.java
+++ HW7/src/BaseAuthService.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BaseAuthService implements AuthService {
-    private class Entry {
+    private class Entry {// создание вложенного класса
         private String login;
         private String password;
-        private String nick;
+        private String nick;// поля
 
-        public Entry(String login, String password, String nick) {
+        public Entry(String login, String password, String nick) { // конструктор вложенного класса
             this.login = login;
             this.password = password;
             this.nick = nick;
@@ -16,7 +19,7 @@ public class BaseAuthService implements AuthService {
 
         public String getLogin() {
             return login;
-        }
+        }// реализация интерфейса у вложенного класса
 
         public String getPassword() {
             return password;
@@ -27,24 +30,26 @@ public class BaseAuthService implements AuthService {
         }
     }
 
-    private List<Entry> entries;
+    private List<Entry> entries; //  List  содержащий образцы вложенного класса
 
     public BaseAuthService() {
         init();
-    }
+    } // создание конструктора основного класса который вызывает  метод init();
 
     public int init() {
         entries = new ArrayList<>();
         entries.add(new Entry("login1", "pass1", "nick1"));
         entries.add(new Entry("login2", "pass2", "nick2"));
         entries.add(new Entry("login3", "pass3", "nick3"));
-        return entries.size();
+        return entries.size();// добавлено три экземпляра класса Entry, возвращает он размер, добавленного в этот Лист
     }
 
-    @Override
+    @Override// реализация интерфейса для основного класса
     public String getNick(String login, String pass) {
         for (Entry e : entries) {
             if (e.getLogin().equals(login) && e.getPassword().equals(pass)) return e.getNick();
+            // если логин и пароль е совпадает с введеными - всё ок. Иначе возвращает null.
+
         }
         return null;
     }
@@ -53,7 +58,7 @@ public class BaseAuthService implements AuthService {
     public boolean login(String login, String pass) {
         for (Entry e : entries) {
             if (e.getLogin().equals(login) && e.getPassword().equals(pass)) return true;
-        }
+        }// если логин и пароль е совпадает с введеными - true. Иначе возвращает false.
         return false;
     }
 
